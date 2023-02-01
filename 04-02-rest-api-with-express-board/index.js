b30fa73a097595c13180efa0cd3d869bfc3b90e5
yjin0034 <yjin0034@naver.com>
2023-02-01 22:20:26 +0900

    06. GraphQL-API 실습 - fetchBoard API, createBoard API, createTokenOfPhone API 생성

--- 04-02-rest-api-with-express-board/index.js
+++ 04-02-rest-api-with-express-board/index.js
@@ -10,24 +10,9 @@ app.use(express.json());
 app.get('/boards', (req, res) => {
   // 1. 데이터를 조회하는 로직 => DB에 접속해서 데이터 꺼내오기
   const result = [
-    {
-      number: 1,
-      writer: '철수',
-      title: '제목입니다~~',
-      contents: '내용이에요@@@',
-    },
-    {
-      number: 2,
-      writer: '영희',
-      title: '영희 제목입니다~~',
-      contents: '영희 내용이에요@@@',
-    },
-    {
-      number: 3,
-      writer: '훈이',
-      title: '훈이 제목입니다~~',
-      contents: '훈이 내용이에요@@@',
-    },
+    { number: 1, writer: '철수', title: '제목입니다~~', contents: '내용이에요@@@' },
+    { number: 2, writer: '영희', title: '영희 제목입니다~~', contents: '영희 내용이에요@@@' },
+    { number: 3, writer: '훈이', title: '훈이 제목입니다~~', contents: '훈이 내용이에요@@@' },
   ];
 
   // 2. 꺼내온 결과 응답 주기
@@ -35,11 +20,12 @@ app.get('/boards', (req, res) => {
 });
 
 app.post('/boards', (req, res) => {
+  // 1. 데이터를 등록하는 로직 => DB에 접속해서 데이터 저장하기
+  // 프론트엔드로부터 데이터 받아오기
+  // 콘솔로 찍어서 확인 해보기
   console.log(req.body);
 
-  // 1. 데이터를 등록하는 로직 => DB에 접속해서 데이터 저장하기
-
-  // 2. 저장 결과 응답 주기
+  // 2. 저장 결과 알려주기
   res.send('게시물 등록에 성공하였습니다!!');
 });
 
