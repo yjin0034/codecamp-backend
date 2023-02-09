c3c2219b5c4c7be0cb77e121c2dca0814f56eeba
yjin0034 <yjin0034@naver.com>
2023-02-09 22:25:30 +0900

    21. MVC 패턴 - in Express

---
+++ 11-05-OOP-coupon/mvc/controllers/product.controller.js
@@ -0,0 +1,32 @@
+import { CashService } from "./services/cash.service.js";
+import { ProductService } from "./services/product.service.js";
+
+export class ProductController {
+  // 상품 구매하기
+  buyProduct = (req, res) => {
+    // 1. 가진 돈 검증하는 코드 (10줄 => 2줄)
+    const cashService = new CashService();
+    const hasMoney = cashService.checkValue();  // true 또는 false 리턴
+  
+    // 2. 판매 여부(판매 처리) 검증하는 코드 (10줄 => 2줄)
+    const productService = new ProductService();
+    const isSoldout = productService.checkSoldout();  // true 또는 false 리턴
+  
+    // 3. 상품 구매하는 코드
+    if(hasMoney && !isSoldout){  // 돈 있고 && 판매 처리된 상품이 아니라면,
+      res.send("상품 구매 완료!!");
+    }
+  }
+
+  // 상품 환불하기
+  refundProduct = (req, res) => {
+    // 1. 판매 여부(판매 처리) 검증하는 코드 (10줄 => 2줄)
+    const productService = new ProductService();
+    const isSoldout = productService.checkSoldout();  // true 또는 false 리턴
+  
+    // 2. 상품 환불하는 코드
+    if(isSoldout) {  // 판매 처리된 상품이라면,
+      res.send("상품 환불 완료!!");
+    }
+  }
+}
