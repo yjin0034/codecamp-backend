1ddec933d73d075c6b3324fe37b1195911b3cd49
yjin0034 <yjin0034@naver.com>
2023-02-15 21:09:16 +0900

    26. Nest.js - MySQL 연결. TypeORM을 활용한 Nestjs와 MySQL 연결, Board Entity 생성

---
+++ 13-02-nestjs-with-typeorm/src/app.module.ts
@@ -0,0 +1,32 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BoardModule } from './apis/boards/boards.module';
+import { Board } from './apis/boards/entities/board.entity';
+//import { AppController } from './app.controller';
+//import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    BoardModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'src/commons/graphql/schema.gql',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql', // 데이터 베이스 타입
+      host: 'localhost', // local 환경으로 진행
+      port: 3306, // mysql은 기본 port는 3306
+      username: 'root', // mysql은 기본 user는 root로 지정
+      password: 'bada332@', // 본인의 mysql password
+      database: 'myproject03', // 연결할 데이터 베이스명
+      entities: [Board], // 데이터 베이스와 연결할 entity
+      synchronize: true, // entity 테이블을 데이터베이스와 동기화할 것인지
+      logging: true, // 콘솔 창에 log를 표시할 것인지
+    }),
+  ],
+  //controllers: [AppController],
+  //providers: [AppService],
+})
+export class AppModule {}
