ced1f84cf9a6a3bcf103c445a35e463d8884b339
yjin0034 <yjin0034@naver.com>
2023-03-08 21:23:35 +0900

    31. 상품 카테고리 등록 API - ProductCategory Create

---
+++ 17-01-typeorm-crud/src/apis/products/entities/product.entity.ts
@@ -0,0 +1,46 @@
+import { ProductCategory } from 'src/apis/productCategory/entites/productCategory.entity';
+import { ProductSaleslocation } from 'src/apis/productSaleslocation/entities/productSaleslocation.entity';
+import { ProductTag } from 'src/apis/productTags/entities/productTag.entity';
+import { User } from 'src/apis/users/entities/user.entity';
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  JoinTable,
+  ManyToMany,
+  ManyToOne,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  price: number;
+
+  @Column()
+  isSoldout: boolean;
+
+  @JoinColumn()
+  @OneToOne(() => ProductSaleslocation)
+  productSaleslocation: ProductSaleslocation;
+
+  @ManyToOne(() => ProductCategory)
+  productCategory: ProductCategory;
+
+  @ManyToOne(() => User)
+  user: User;
+
+  @JoinTable()
+  @ManyToMany(() => ProductTag, (productTags) => productTags.products)
+  productTags: ProductTag[];
+}
---
+++ 17-01-typeorm-crud/src/app.module.ts
@@ -0,0 +1,29 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { BoardModule } from './apis/boards/boards.module';
+import { ProductCategoryModule } from './apis/productCategory/productCategory.module';
+
+@Module({
+  imports: [
+    BoardModule,
+    ProductCategoryModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: 'src/commons/graphql/schema.gql',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql', // 데이터 베이스 타입
+      host: 'localhost', // local 환경으로 진행
+      port: 3306, // mysql은 기본 port는 3306
+      username: 'root', // mysql은 기본 user는 root로 지정
+      password: 'bada332@', // 본인의 mysql password
+      database: 'myproject03', // 연결할 데이터 베이스명
+      entities: [__dirname + '/apis/**/*.entity.*'], // 데이터 베이스와 연결할 entity
+      synchronize: true, // entity 테이블을 데이터베이스와 동기화할 것인지
+      logging: true, // 콘솔 창에 log를 표시할 것인지
+    }),
+  ],
+})
+export class AppModule {}
