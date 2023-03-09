b20a92950638257aa6a93173291fb404d75a5bdf
yjin0034 <yjin0034@naver.com>
2023-03-09 16:01:32 +0900

    33. 상품 조회, 수정 API - Product Read(Fetch), Product Update, Product Update With Error

--- 17-01-typeorm-crud/src/apis/products/product.resolver.ts
+++ 17-01-typeorm-crud/src/apis/products/product.resolver.ts
@@ -1,5 +1,6 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CreateProductInput } from './dto/createProduct.input';
+import { UpdateProductInput } from './dto/updateProduct.input';
 import { Product } from './entities/product.entity';
 import { ProductService } from './product.service';
 
@@ -7,10 +8,34 @@ import { ProductService } from './product.service';
 export class ProductResolver {
   constructor(private readonly productService: ProductService) {}
 
+  @Query(() => [Product])
+  fetchProducts() {
+    return this.productService.findAll();
+  }
+
+  @Query(() => Product)
+  fetchProduct(
+    @Args('productId') productId: string, //
+  ) {
+    return this.productService.findOne({ productId });
+  }
+
   @Mutation(() => Product)
   async createProduct(
     @Args('createProductInput') createProductInput: CreateProductInput,
   ) {
     return await this.productService.create({ createProductInput });
+  }
+
+  @Mutation(() => Product)
+  async updateProduct(
+    @Args('productId') productId: string,
+    @Args('updateProductInput') updateProductInput: UpdateProductInput,
+  ) {
+    // 판매 완료가 되었는지 확인해보기
+    await this.productService.checkSoldout({ productId });
+
+    // 수정하기
+    return await this.productService.update({ productId, updateProductInput });
   }
 }
--- 17-01-typeorm-crud/src/apis/products/product.service.ts
+++ 17-01-typeorm-crud/src/apis/products/product.service.ts
@@ -1,4 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  UnprocessableEntityException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Product } from './entities/product.entity';
@@ -10,6 +15,14 @@ export class ProductService {
     private readonly productRepository: Repository<Product>,
   ) {}
 
+  async findAll() {
+    return await this.productRepository.find();
+  }
+
+  async findOne({ productId }) {
+    return await this.productRepository.findOne({ where: { id: productId } });
+  }
+
   async create({ createProductInput }) {
     const result = await this.productRepository.save({
       ...createProductInput, // 스프레드 연산자 사용하기
@@ -20,5 +33,40 @@ export class ProductService {
       // price: createProductInput.price,
     });
     return result;
+  }
+
+  async update({ productId, updateProductInput }) {
+    const myproduct = await this.productRepository.findOne({
+      where: { id: productId },
+    });
+
+    const newProduct = {
+      ...myproduct,
+      id: productId,
+      ...updateProductInput, // 스프레드 연산자 사용하기
+
+      // 하나하나 직접 나열하는 방식
+      // name: updateProductInput.name,
+      // price: updateProductInput.price
+    };
+
+    return await this.productRepository.save(newProduct);
+  }
+
+  async checkSoldout({ productId }) {
+    const product = await this.productRepository.findOne({
+      where: { id: productId },
+    });
+
+    if (product.isSoldout) {
+      throw new UnprocessableEntityException('이미 판매 완료된 상품입니다.');
+    }
+
+    // if (product.isSoldout) {
+    //   throw new HttpException(
+    //     '이미 판매 완료된 상품입니다.',
+    //     HttpStatus.UNPROCESSABLE_ENTITY,
+    //   );
+    // }
   }
 }
