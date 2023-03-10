52b2849721581e239b5fe0439e0fa474a24368a7
yjin0034 <yjin0034@naver.com>
2023-03-11 02:54:37 +0900

    39. 회원가입 API 보완 - User Create. Hash 암호화 적용 - Hashing 라이브러리 Bcrypt 활용

---
+++ 20-01-signup-bcrypt/src/apis/products/entities/product.entity.ts
@@ -0,0 +1,61 @@
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { ProductCategory } from 'src/apis/productCategory/entities/productCategory.entity';
+import { ProductSaleslocation } from 'src/apis/productSaleslocation/entities/productSaleslocation.entity';
+import { ProductTag } from 'src/apis/productTags/entities/productTag.entity';
+import { User } from 'src/apis/users/entities/user.entity';
+import {
+  Column,
+  DeleteDateColumn,
+  Entity,
+  JoinColumn,
+  JoinTable,
+  ManyToMany,
+  ManyToOne,
+  OneToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
+@ObjectType()
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  @Field(() => String)
+  id: string;
+
+  @Column()
+  @Field(() => String)
+  name: string;
+
+  @Column()
+  @Field(() => String)
+  description: string;
+
+  @Column()
+  @Field(() => Int)
+  price: number;
+
+  @Column({ default: false })
+  @Field(() => Boolean)
+  isSoldout: boolean;
+
+  @DeleteDateColumn()
+  deletedAt: Date;
+
+  @JoinColumn()
+  @OneToOne(() => ProductSaleslocation)
+  @Field(() => ProductSaleslocation)
+  productSaleslocation: ProductSaleslocation;
+
+  @ManyToOne(() => ProductCategory)
+  @Field(() => ProductCategory)
+  productCategory: ProductCategory;
+
+  @ManyToOne(() => User)
+  @Field(() => User)
+  user: User;
+
+  @JoinTable()
+  @ManyToMany(() => ProductTag, (productTags) => productTags.products)
+  @Field(() => [ProductTag])
+  productTags: ProductTag[];
+}
