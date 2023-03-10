b713509f15b0da4c62a06f5320fde99848eea885
yjin0034 <yjin0034@naver.com>
2023-03-11 02:19:34 +0900

    38. 회원가입 API - User Create

---
+++ 19-02-signup/src/apis/users/user.resolver.ts
@@ -0,0 +1,21 @@
+import { Args, Mutation, Resolver } from "@nestjs/graphql";
+import { User } from "./entities/user.entity";
+import { UserService } from "./user.service";
+
+@Resolver()
+export class UserResolver {
+  constructor(
+    private readonly userService: UserService, //
+  ) {}
+
+  @Mutation(() => User)
+  createUser(
+    @Args('email') email: string,
+    @Args('password') password: string,
+    @Args('name') name: string,
+    @Args('age') age: number,
+  ){
+    return this.userService.create({ email, password, name, age });
+  }
+}
+
